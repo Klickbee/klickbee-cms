1cb50cdfcf0048e72b7e22c7e86e257f4e06d5aa
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-08-04 15:28:47 +0200

    feat: reorder export statement for Page type in page.ts

--- src/feature/page/types/page.ts
+++ src/feature/page/types/page.ts
@@ -1,5 +1,5 @@
-export type { Page } from "@/generated/prisma";
-
 import type { Page } from "@/generated/prisma";
+
+export type { Page } from "@/generated/prisma";
 
 export type PageLight = Pick<Page, "id" | "title" | "slug">;
