6e7d8f5badaec4e784e8f01815224e8c6cdde649
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-08-04 15:32:02 +0200

    feat: remove homepage ID fetching from BuilderPage for simplified component usage

--- src/app/admin/[adminKey]/builder/page.tsx
+++ src/app/admin/[adminKey]/builder/page.tsx
@@ -4,17 +4,5 @@ import { PageLight } from "@/feature/page/types/page";
 import { getSetting } from "@/feature/settings/lib/settings";
 
 export default async function BuilderPage() {
-	// Fetch homepage ID from settings
-	const homePageIdSetting = await getSetting("current_homepage_id");
-	const homePageId = homePageIdSetting
-		? Number(homePageIdSetting)
-		: undefined;
-
-	// Fetch page data if we have a homepage ID
-	let pageData: PageLight | undefined;
-	if (homePageId) {
-		pageData = (await getPageById(homePageId)) as PageLight;
-	}
-
-	return <BuilderComponent homePageId={homePageId} pageData={pageData} />;
+	return <BuilderComponent />;
 }
