ea53a00dac8d49995ebfbf405c102384a4c5c5a8
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-08-07 15:21:01 +0200

    feat: extend DragDropContext with sourceComponent and reorderComponents

--- src/components/builder/_partials/DragAndDropContext.tsx
+++ src/components/builder/_partials/DragAndDropContext.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { ComponentItem } from "@/builder/definitions/componentsList";
 
 export const DragDropContext = React.createContext<{
 	targetComponent: string | null;
 	setTargetComponent: (id: string | null) => void;
+	sourceComponent: ComponentItem | null;
+	setSourceComponent: (component: ComponentItem | null) => void;
+	reorderComponents: (
+		parentId: string | null,
+		sourceId: string,
+		targetId: string,
+	) => void;
 } | null>(null);
