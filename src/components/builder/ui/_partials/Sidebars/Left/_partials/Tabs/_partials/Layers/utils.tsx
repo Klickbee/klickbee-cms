d83b48473468e65219ba2b48af01ded428043313
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-08-08 10:40:47 +0200

    feat: update component identification logic in utils for improved accuracy

--- src/components/builder/ui/_partials/Sidebars/Left/_partials/Tabs/_partials/Layers/utils.tsx
+++ src/components/builder/ui/_partials/Sidebars/Left/_partials/Tabs/_partials/Layers/utils.tsx
@@ -7,7 +7,7 @@ import { BaseComponent } from "@/builder/types/components/component";
  */
 export const getComponentIcon = (component: BaseComponent) => {
 	const componentDef = componentsList.find(
-		(c: BaseComponent) => c.id === component.type,
+		(c: BaseComponent) => c.id === component.label,
 	);
 	return componentDef ? componentDef.icon : <Box className="w-4 h-4" />;
 };
@@ -17,7 +17,7 @@ export const getComponentIcon = (component: BaseComponent) => {
  */
 export const mapContentToTree = (content: BaseComponent[]): BaseComponent[] => {
 	return content.map((component) => {
-		const base = componentsList.find((c) => c.id === component.groupId) || {
+		const base = componentsList.find((c) => c.type === component.type) || {
 			group: component.groupId || "",
 			icon: getComponentIcon(component),
 			id: component.id,
