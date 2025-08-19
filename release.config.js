d34adf2e5786931e2df8a2c09591a09bbe6a4075
nadmax <maximilien.nadji@pm.me>
2025-08-19 16:39:47 +0200

    feat: improve changelog generation with semantic-release configuration

--- release.config.js
+++ release.config.js
@@ -1,11 +1,68 @@
 export default {
 	branches: ["main"],
 	plugins: [
-		"@semantic-release/commit-analyzer",
-		"@semantic-release/release-notes-generator",
+		[
+			"@semantic-release/commit-analyzer",
+			{
+				preset: "conventionalcommits",
+				releaseRules: [
+					{ release: "minor", type: "feat" },
+					{ release: "patch", type: "fix" },
+					{ release: "patch", type: "perf" },
+					{ release: "patch", type: "docs" },
+					{ release: "patch", type: "style" },
+					{ release: "patch", type: "refactor" },
+					{ release: "patch", type: "test" },
+					{ release: "patch", type: "chore" },
+				],
+			},
+		],
+		[
+			"@semantic-release/release-notes-generator",
+			{
+				preset: "conventionalcommits",
+				presetConfig: {
+					types: [
+						{ section: "✨ Features", type: "feat" },
+						{ section: "🐛 Bug Fixes", type: "fix" },
+						{
+							section: "⚡ Performance Improvements",
+							type: "perf",
+						},
+						{ section: "⏪ Reverts", type: "revert" },
+						{
+							hidden: true,
+							section: "📚 Documentation",
+							type: "docs",
+						},
+						{ hidden: true, section: "💎 Styles", type: "style" },
+						{
+							hidden: true,
+							section: "🔧 Miscellaneous Chores",
+							type: "chore",
+						},
+						{
+							hidden: true,
+							section: "📦 Code Refactoring",
+							type: "refactor",
+						},
+						{ hidden: true, section: "🚨 Tests", type: "test" },
+						{ hidden: true, section: "🛠 Builds", type: "build" },
+						{
+							hidden: true,
+							section: "⚙️ Continuous Integrations",
+							type: "ci",
+						},
+					],
+				},
+				writerOpts: {
+					commitGroupsSort: "title",
+					commitsSort: ["scope", "subject"],
+					groupBy: "type",
+				},
+			},
+		],
 		"@semantic-release/changelog",
-		"@semantic-release/npm",
 		"@semantic-release/github",
 	],
-	preset: "conventionalcommits",
 };
