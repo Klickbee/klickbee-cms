2a5dd0839f8461516cc87340bf8020cd775c3ad2
Lucas <allard.lucas.97@gmail.com>
2025-09-13 13:21:55 +0200

    refactor: set default currentTab to "Pages" in useCurrentTabStore for improved usability

--- src/builder/store/storeCurrentTabsSidebar.ts
+++ src/builder/store/storeCurrentTabsSidebar.ts
@@ -8,6 +8,6 @@ type CurrentTabStore = {
 
 export const useCurrentTabStore = create<CurrentTabStore>((set) => ({
 	clearCurrentTab: () => set({ currentTab: "" }),
-	currentTab: "",
+	currentTab: "Pages", // Default to Pages tab
 	setCurrentTab: (tab: string) => set({ currentTab: tab }),
 }));
