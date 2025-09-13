d3147f78ba4d6e5fe2def7c1c631d80a3bf352db
Lucas <allard.lucas.97@gmail.com>
2025-09-13 13:17:16 +0200

    refactor: fix useLastPageId to properly pass lastPageIdOptions

--- src/feature/page/queries/useLastPageId.ts
+++ src/feature/page/queries/useLastPageId.ts
@@ -2,5 +2,5 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { lastPageIdOptions } from "@/feature/page/options/lastPageIdOptions";
 
 export function useLastPageId() {
-	return useSuspenseQuery(lastPageIdOptions());
+	return useSuspenseQuery(lastPageIdOptions);
 }
