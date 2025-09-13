059bc01ed0d32993adcc6057056c4567e9d4ee3c
Lucas <allard.lucas.97@gmail.com>
2025-09-13 13:15:17 +0200

    feat: add BuilderContentProperties component to BuilderRightSidebar for enhanced functionality #115

--- src/components/builder/ui/BuilderRightSidebar.tsx
+++ src/components/builder/ui/BuilderRightSidebar.tsx
@@ -1,8 +1,10 @@
+import BuilderContentProperties from "./_partials/Sidebars/Right/ContentProperties";
 import BuilderStyleProperties from "./_partials/Sidebars/Right/StyleProperties";
 
 export default function BuilderRightSidebar() {
 	return (
 		<div className="border-l border-gray-200 h-[calc(100vh_-4rem)] flex flex-col overflow-scroll [scrollbar-width:none] [&::-webkit-scrollbar]:hidden;">
+			<BuilderContentProperties />
 			<BuilderStyleProperties />
 		</div>
 	);
