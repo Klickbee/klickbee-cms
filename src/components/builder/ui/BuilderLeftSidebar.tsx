f9f0a33fd5219a26db332daa6b103768a94dc2d1
Lucas <allard.lucas.97@gmail.com>
2025-09-13 13:14:49 +0200

    feat: remove unused state management logic from BuilderLeftSidebar for cleanup #115

--- src/components/builder/ui/BuilderLeftSidebar.tsx
+++ src/components/builder/ui/BuilderLeftSidebar.tsx
@@ -1,17 +1,7 @@
-"use client";
-import { useEffect } from "react";
-import { useCurrentTabStore } from "@/builder/store/storeCurrentTabsSidebar";
 import BuilderTabContent from "@/components/builder/ui/_partials/Sidebars/Left/TabContent";
 import BuilderTabSwitcher from "@/components/builder/ui/_partials/Sidebars/Left/TabSwitcher";
 
 export default function BuilderLeftSidebar() {
-	// Use useEffect to set the initial tab state after component mounts
-	useEffect(() => {
-		useCurrentTabStore.setState({
-			currentTab: "Pages",
-		});
-	}, []);
-
 	return (
 		<>
 			<BuilderTabSwitcher />
