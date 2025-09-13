6429a3994c9a0a0f26b30c0b940bac9a0718243a
Lucas <allard.lucas.97@gmail.com>
2025-09-13 13:22:44 +0200

    refactor: add React Query hydration to BuilderPage for server-side data prefetching

--- src/app/admin/[adminKey]/builder/page.tsx
+++ src/app/admin/[adminKey]/builder/page.tsx
@@ -1,16 +1,27 @@
+import { dehydrate } from "@tanstack/query-core";
+import { HydrationBoundary } from "@tanstack/react-query";
 import { DeleteComponentProvider } from "@/builder/contexts/DeleteComponentContext";
 import BuilderComponent from "@/components/builder/Builder";
 import BuilderHeader from "@/components/builder/ui/BuilderHeader";
 import { Toaster } from "@/components/ui/sonner";
+import { allPagesOptions } from "@/feature/page/options/allPagesOptions";
+import { lastPageIdOptions } from "@/feature/page/options/lastPageIdOptions";
+import { getQueryClient } from "@/lib/getQueryClient";
 
 export default async function BuilderPage() {
+	const queryClient = getQueryClient();
+	void queryClient.prefetchQuery(allPagesOptions);
+	void queryClient.prefetchQuery(lastPageIdOptions);
+
 	return (
-		<DeleteComponentProvider>
-			<div className="flex flex-col h-full">
-				<BuilderHeader />
-				<BuilderComponent />
-			</div>
-			<Toaster position={"bottom-center"} />
-		</DeleteComponentProvider>
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<DeleteComponentProvider>
+				<div className="flex flex-col h-full">
+					<BuilderHeader />
+					<BuilderComponent />
+				</div>
+				<Toaster position={"bottom-center"} />
+			</DeleteComponentProvider>
+		</HydrationBoundary>
 	);
 }
