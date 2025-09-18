1e8e9e6e09c42061db61e6fd47d93c2114dcd80e
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-09-18 17:32:27 +0200

    refactor: enhance layout of BuilderLeftSidebar for better structure and overflow handling

--- src/components/builder/ui/BuilderLeftSidebar.tsx
+++ src/components/builder/ui/BuilderLeftSidebar.tsx
@@ -3,9 +3,11 @@ import BuilderTabSwitcher from "@/components/builder/ui/_partials/Sidebars/Left/
 
 export default function BuilderLeftSidebar() {
 	return (
-		<>
+		<div className="border-r border-gray-200 h-[calc(100vh_-5rem)] flex flex-col overflow-hidden">
 			<BuilderTabSwitcher />
-			<BuilderTabContent />
-		</>
+			<div className="flex-1 min-h-0">
+				<BuilderTabContent />
+			</div>
+		</div>
 	);
 }
