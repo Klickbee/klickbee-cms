88dae5cce393d8aded39cb829d8f814e6a7c0bb6
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-09-18 17:31:51 +0200

    feat: wrap tab content in ScrollArea for improved layout and scrolling

--- src/components/builder/ui/_partials/Sidebars/Left/TabContent.tsx
+++ src/components/builder/ui/_partials/Sidebars/Left/TabContent.tsx
@@ -4,11 +4,15 @@ import { useCurrentTabStore } from "@/builder/store/storeCurrentTabsSidebar";
 import BuilderTabComponents from "@/components/builder/ui/_partials/Sidebars/Left/_partials/Tabs/Components";
 import BuilderTabLayers from "@/components/builder/ui/_partials/Sidebars/Left/_partials/Tabs/Layers";
 import BuilderTabPages from "@/components/builder/ui/_partials/Sidebars/Left/_partials/Tabs/Pages";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function BuilderTabContent() {
 	const activeTab = useCurrentTabStore((state) => state.currentTab);
-	if (activeTab === "Pages") return <BuilderTabPages />;
-	if (activeTab === "Layers") return <BuilderTabLayers />;
-	if (activeTab === "Components") return <BuilderTabComponents />;
-	return null;
+
+	let content = null as unknown as React.ReactNode;
+	if (activeTab === "Pages") content = <BuilderTabPages />;
+	else if (activeTab === "Layers") content = <BuilderTabLayers />;
+	else if (activeTab === "Components") content = <BuilderTabComponents />;
+
+	return <ScrollArea className="h-full w-full">{content}</ScrollArea>;
 }
