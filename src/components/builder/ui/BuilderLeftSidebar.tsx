9b56df08b62763923bc77b343f2918f5e0f2068d
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-09-19 10:39:47 +0200

    feat: expand width of BuilderLeftSidebar for better layout

--- src/components/builder/ui/BuilderLeftSidebar.tsx
+++ src/components/builder/ui/BuilderLeftSidebar.tsx
@@ -3,7 +3,7 @@ import BuilderTabSwitcher from "@/components/builder/ui/_partials/Sidebars/Left/
 
 export default function BuilderLeftSidebar() {
 	return (
-		<div className="border-r border-gray-200 h-[calc(100vh_-5rem)] flex flex-col overflow-hidden">
+		<div className="border-r border-gray-200 h-[calc(100vh_-5rem)] flex flex-col overflow-hidden w-full">
 			<BuilderTabSwitcher />
 			<div className="flex-1 min-h-0">
 				<BuilderTabContent />
