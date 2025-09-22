fd1bf90b11a4a5e9cc5b57f3906971371637bd40
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-09-22 16:10:20 +0200

    feat: add server actions body size limit to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
+	experimental: {
+		serverActions: {
+			bodySizeLimit: "256mb",
+		},
+	},
 };
 
 const withNextIntl = createNextIntlPlugin(
