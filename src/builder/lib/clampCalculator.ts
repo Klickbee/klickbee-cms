be3b8228ec13722b645328d5867e20811dda0df7
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-09-22 18:12:26 +0200

    feat: enhance clampCalculator functions for improved size handling and unit support

--- src/builder/lib/clampCalculator.ts
+++ src/builder/lib/clampCalculator.ts
@@ -4,12 +4,23 @@ import { FluidSize } from "@/builder/types/settings/FluidSize";
 export function toClamp(size: FluidSize, breakpointMaxWidth?: number): string {
 	const maxWidth = breakpointMaxWidth ?? getBuilderMaxWidth() ?? 1440;
 
-	if (size.max == size.min) return `${size.max}${size.sizeUnit}`;
-	if (size.min == undefined) {
-		size.min = compressNumber(size.max);
+	const rootFontSize =
+		parseFloat(getComputedStyle(document.documentElement).fontSize) || 16;
+
+	if (size.max == size.min) {
+		size.min = compressNumber(size.max, size.sizeUnit, rootFontSize);
 	}
-	if (size.max == undefined) {
-		size.max = expandNumber(size.min);
+	if (size.min == undefined) {
+		size.min = compressNumber(size.max, size.sizeUnit, rootFontSize);
+	}
+	if ((size.max == undefined || size.max == 0) && size.min > 0) {
+		size.max = expandNumber(size.min, size.sizeUnit, rootFontSize);
+	}
+
+	if (size.max < size.min) {
+		const temp = size.max;
+		size.max = size.min;
+		size.min = temp;
 	}
 
 	const min = size.min;
@@ -17,13 +28,12 @@ export function toClamp(size: FluidSize, breakpointMaxWidth?: number): string {
 
 	const sizeUnit = size.sizeUnit;
 
+	// console.log({min, max, maxWidth, sizeUnit})
+
 	let slope: number;
 	if (sizeUnit === "rem" && typeof window !== "undefined") {
-		const rootFontSize =
-			parseFloat(getComputedStyle(document.documentElement).fontSize) ||
-			16;
 		slope = max / (maxWidth / rootFontSize);
-	} else if (sizeUnit === "px" && typeof window !== "undefined") {
+	} else if (sizeUnit === "px") {
 		slope = max / maxWidth;
 	} else {
 		return `${max}${sizeUnit}`;
@@ -32,12 +42,34 @@ export function toClamp(size: FluidSize, breakpointMaxWidth?: number): string {
 	return `clamp(${min}${sizeUnit}, ${(slope * 100).toFixed(4)}cqw, ${max}${sizeUnit})`;
 }
 
-function compressNumber(x: number, p = 0.35): number {
-	if (x >= 1) return Math.pow(x, p); // x > 1 → racine p
-	return 1 - Math.pow(1 - x, p); // x < 1 → symétrique vers le bas
+function compressNumber(
+	x: number,
+	sizeUnit?: string,
+	rootSize?: number,
+): number {
+	let multiplier;
+	const p = 0.9; // facteur de puissance
+	if (sizeUnit === "px" && rootSize) {
+		x = x / rootSize;
+	}
+	if (x >= 1)
+		multiplier = Math.pow(x, p); // x > 1 → racine p
+	else multiplier = 1 - Math.pow(1 - x, p); // x < 1 → symétrique vers le bas
+	if (sizeUnit === "px" && rootSize) {
+		return multiplier * rootSize;
+	} else return multiplier;
 }
 
-function expandNumber(x: number, p = 0.35): number {
-	if (x >= 1) return Math.pow(x, 1 / p); // x > 1
-	return 1 - Math.pow(1 - x, 1 / p); // x < 1
+function expandNumber(x: number, sizeUnit?: string, rootSize?: number): number {
+	const p = 1.9; // facteur de puissance
+	let multiplier;
+	if (sizeUnit === "px" && rootSize) {
+		x = x / rootSize;
+	}
+	if (x >= 1)
+		multiplier = Math.pow(x, 1 / p); // x > 1
+	else multiplier = 1 - Math.pow(1 - x, 1 / p); // x < 1
+	if (sizeUnit === "px" && rootSize) {
+		return multiplier * rootSize;
+	} else return multiplier;
 }
