c76bb0d79fcbc2da1c954cccd84c396bc20b77d0
lucas <allard.lucas.97@gmail.com>
2025-09-24 11:13:11 +0200

    refactor(contact): migrate contact page to use cardListLayout (#155)

--- src/app/admin/[adminKey]/(dashboard)/manage/contact/page.tsx
+++ src/app/admin/[adminKey]/(dashboard)/manage/contact/page.tsx
@@ -1,16 +1,18 @@
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { useTranslations } from "next-intl";
+import CardListLayout from "@/components/admin/_partials/cardListLayout";
 import DashboardTitle from "@/components/admin/_partials/dashboardTitle";
-import CardTitle from "@/components/admin/manage/CardTitle";
 import ContactActionButton from "@/components/admin/manage/contact/contactActionButton";
 import ContactSearchBar from "@/components/admin/manage/contact/contactSearchBar";
 import ContactsPagination from "@/components/admin/manage/contact/contactsPagination";
 import ContactsTable from "@/components/admin/manage/contact/contactsTable";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { ContactsTableProvider } from "@/feature/contact/contexts/ContactsTableContext";
 import { allContactsOptions } from "@/feature/contact/options/allContactsOptions";
 import { getQueryClient } from "@/lib/getQueryClient";
 
 export default function Page() {
+	const t = useTranslations("Contacts");
+
 	const queryClient = getQueryClient();
 	void queryClient.prefetchQuery(allContactsOptions);
 
@@ -22,19 +24,16 @@ export default function Page() {
 				translationNamespace="Contacts"
 			/>
 			<section className="py-6 px-8">
-				<Card className="gap-0 py-0">
-					<CardHeader className="py-3 px-4 gap-0 flex flex-row justify-between items-center border-b">
-						<CardTitle>Form submission</CardTitle>
-						<ContactActionButton />
-					</CardHeader>
-					<CardContent className="p-4 flex flex-col gap-4">
-						<ContactsTableProvider>
-							<ContactSearchBar />
-							<ContactsTable />
-							<ContactsPagination />
-						</ContactsTableProvider>
-					</CardContent>
-				</Card>
+				<ContactsTableProvider>
+					<CardListLayout
+						actionButtons={<ContactActionButton />}
+						searchBar={<ContactSearchBar />}
+						title={t("ManageContacts")}
+					>
+						<ContactsTable />
+						<ContactsPagination />
+					</CardListLayout>
+				</ContactsTableProvider>
 			</section>
 		</HydrationBoundary>
 	);
