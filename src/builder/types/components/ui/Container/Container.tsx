9b24f4b02a2081a47dcdbf1c3a1ef69bb466a701
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-10-29 17:03:17 +0100

    feat: add default margin and padding settings to Container component

--- src/builder/types/components/ui/Container/Container.tsx
+++ src/builder/types/components/ui/Container/Container.tsx
@@ -13,7 +13,22 @@ export const ContainerBC: ContainerBCI = {
 			bordersAndCorners: {},
 			effects: {},
 			layout: {},
-			sizeAndSpacing: {},
+			sizeAndSpacing: {
+				margin: {
+					bottom: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
+					key: "default-margin",
+					left: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
+					right: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
+					top: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
+				},
+				padding: {
+					bottom: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
+					key: "default-padding",
+					left: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
+					right: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
+					top: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
+				},
+			},
 		},
 	},
 	type: "container",
