9735b1aa2131b30366932903b6fb02a857482495
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-10-29 17:46:51 +0100

    feat: remove unused margin properties from sizeAndSpacing in Container component

--- src/builder/types/components/ui/Container/Container.tsx
+++ src/builder/types/components/ui/Container/Container.tsx
@@ -14,13 +14,6 @@ export const ContainerBC: ContainerBCI = {
 			effects: {},
 			layout: {},
 			sizeAndSpacing: {
-				margin: {
-					bottom: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
-					key: "default-margin",
-					left: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
-					right: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
-					top: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
-				},
 				padding: {
 					bottom: { max: 0, maxWidth: 1440, min: 0, sizeUnit: "px" },
 					key: "default-padding",
